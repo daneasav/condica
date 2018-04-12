77848b135f63b1778f334894d185a6b997710f46
Dan Daneasa <victor.daneasa@haufe-lexware.com>
2018-04-12 17:37:11 +0300

    another test

--- azure.tf
+++ azure.tf
@@ -2,9 +2,10 @@
 variable "azure-credentials" {
   type = "map"
   default = {
-    subscription_id = "f3d364a5-0d95-48de-a207-54dd4fb781cd"
-    client_id       = "ecd8572e-cd33-4640-8be6-e8665d6cbe1e"
-    client_secret   = "cbd65a5b-6ed9-44d8-9a48-06796d1dafbf"
+    subscription_id = "1ea27b67-8bb9-4189-9a99-c7fff25c2f67"
+    client_id       = "964c4a41-fc2f-47b6-be26-688dd6a80574"
+    #client_secret   = "W/Hn0ztWK3v1uQmf5vhGOFCctI3mHzMeEdS1ii0133I=
+    client_secret   = "a494bf36-3e4e-11e8-b467-0ed5f89f718b"
     tenant_id       = "88d0a5a9-d61a-4dd6-9a75-6637f3fe6bd8"
   }
 }
@@ -15,4 +16,10 @@ provider "azurerm" {
   client_id       = "${var.azure-credentials["client_id"]}"
   client_secret   = "${var.azure-credentials["client_secret"]}"
   tenant_id       = "${var.azure-credentials["tenant_id"]}"
+}
+
+# create a resource group 
+resource "azurerm_resource_group" "idesk-dns" {
+    name = "condik"
+    location = "northeurope"
 }
--- terraform.sh
+++ terraform.sh
@@ -5,16 +5,16 @@ terraform_image_name="hashicorp/terraform"
 terraform_image_version="0.11.6"
 
 docker volume create "${terraform_volume_name}"
-#docker run -it \
-#  -v "${terraform_volume_name}":/.terraform:rw \
-#  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
-#  "${terraform_image_name_":"${_}" init /tmp
-#docker run -it \
-#  -v "${terraform_volume_name}":/.terraform:rw \
-#  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
-#  "${terraform_image_name_":"${_}" plan /tmp
-#docker run -it \
-#  -v "${terraform_volume_name}":/.terraform:rw \
-#  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
-#  "${terraform_image_name_":"${_}" apply /tmp
+docker run -it \
+  -v "${terraform_volume_name}":/.terraform:rw \
+  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
+  "${terraform_image_name}":"${terraform_image_version}" init /tmp
+docker run -it \
+  -v "${terraform_volume_name}":/.terraform:rw \
+  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
+  "${terraform_image_name}":"${terraform_image_version}" plan /tmp
+docker run -it \
+  -v "${terraform_volume_name}":/.terraform:rw \
+  -v "$(pwd)"/azure.tf:/tmp/azure.tf:ro \
+  "${terraform_image_name}":"${terraform_image_version}" apply /tmp
 docker volume rm "${terraform_volume_name}"
